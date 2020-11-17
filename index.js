c21632d3322e53ca174c77f1c556d6240f0d0802
Shoyas <md.nasiruddin4067@gmail.com>
2020-11-17 18:24:52 +0600

    add message from database

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(cors());
 
 const port = 5000;
 
-
+app.get('/', (req, res) => {
+  res.send('Hello from power-X!')
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true  });
 client.connect(err => {
